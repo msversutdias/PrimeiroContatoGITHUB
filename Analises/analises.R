808c799c31cd8bbb767ea049b7c13db2bca49691
MSD <msdias@unb.br>
2023-06-25 21:25:45 -0300

    inclui npMANOVA

--- Analises/analises.R
+++ Analises/analises.R
@@ -4,3 +4,21 @@ x<-rnorm(100)
 
 plot(x)
 
+library("vegan")
+
+data(dune)
+data(dune.env)
+## default test by terms
+adonis2(dune ~ Management*A1, data = dune.env)
+## overall tests
+adonis2(dune ~ Management*A1, data = dune.env, by = NULL)
+
+### Example of use with strata, for nested (e.g., block) designs.
+dat <- expand.grid(rep=gl(2,1), NO3=factor(c(0,10)),field=gl(3,1) )
+dat
+Agropyron <- with(dat, as.numeric(field) + as.numeric(NO3)+2) +rnorm(12)/2
+Schizachyrium <- with(dat, as.numeric(field) - as.numeric(NO3)+2) +rnorm(12)/2
+total <- Agropyron + Schizachyrium
+dotplot(total ~ NO3, dat, jitter.x=TRUE, groups=field,
+        type=c('p','a'), xlab="NO3", auto.key=list(columns=3, lines=TRUE) )
+
